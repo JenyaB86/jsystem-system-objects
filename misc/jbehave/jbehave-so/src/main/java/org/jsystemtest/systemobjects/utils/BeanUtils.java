03eccc39395f39b5de0afe999f489edbbd3c6ba3
unknown <itaya@UDI-PC.Toyga.local>
2014-11-17 18:38:34 +0200

    Adding the jbehave system object

---
+++ misc/jbehave/jbehave-so/src/main/java/org/jsystemtest/systemobjects/utils/BeanUtils.java
@@ -0,0 +1,110 @@
+package org.jsystemtest.systemobjects.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import jsystem.utils.StringUtils;
+
+public class BeanUtils {
+	/**
+	 * Scans all classes accessible from the context class loader which belong
+	 * to the given package and subpackages.
+	 * 
+	 * @param packageName
+	 *            The base package
+	 * @return The classes
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	public static Class<?>[] getClasses(String packageName) throws ClassNotFoundException, IOException {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		assert classLoader != null;
+		String path = packageName.replace('.', '/');
+		Enumeration<URL> resources = classLoader.getResources(path);
+		List<File> dirs = new ArrayList<File>();
+		while (resources.hasMoreElements()) {
+			URL resource = resources.nextElement();
+			dirs.add(new File(resource.getFile()));
+		}
+		List<Class<?>> classes = new ArrayList<Class<?>>();
+		for (File directory : dirs) {
+			classes.addAll(findClasses(directory, packageName));
+		}
+		return classes.toArray(new Class[classes.size()]);
+	}
+
+	public static boolean isInClasspath(String resourceName) {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		URL resource = classLoader.getResource(resourceName);
+		return resource != null;
+	}
+
+	/**
+	 * Recursive method used to find all classes in a given directory and
+	 * subdirs.
+	 * 
+	 * @param directory
+	 *            The base directory
+	 * @param packageName
+	 *            The package name for classes found inside the base directory
+	 * @return The classes
+	 * @throws ClassNotFoundException
+	 */
+	public static List<Class<?>> findClasses(File directory, String packageName) throws ClassNotFoundException {
+		List<Class<?>> classes = new ArrayList<Class<?>>();
+		if (!directory.exists()) {
+			return classes;
+		}
+		File[] files = directory.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				assert !file.getName().contains(".");
+				classes.addAll(findClasses(file, packageName + "." + file.getName()));
+			} else if (file.getName().endsWith(".class")) {
+				classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+			}
+		}
+		return classes;
+	}
+
+	public static List<Object> getAsInstances(List<Class<?>> classList) {
+		List<Object> instances = new ArrayList<Object>();
+		for (Class<?> clazz : classList) {
+			try {
+				Object instance = clazz.newInstance();
+				instances.add(instance);
+			} catch (Exception e) {
+				System.out.println("Failed to create instance of type " + clazz.getName());
+			}
+		}
+		return instances;
+	}
+
+	/**
+	 * Finds all the classes in the specified packages
+	 * 
+	 * @return List of classes that holds the steps.
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	public static List<Class<?>> findClassesInPackages(String[] packagesArray) {
+		List<Class<?>> classList = new ArrayList<Class<?>>();
+		for (String packageName : packagesArray) {
+			if (!StringUtils.isEmpty(packageName)) {
+				try {
+					for (Class<?> clazz : BeanUtils.getClasses(packageName)) {
+						classList.add(clazz);
+					}
+				} catch (Exception e) {
+					System.out.println("Failed to create find classes in package " + packageName);
+				}
+			}
+		}
+		return classList;
+	}
+
+}
---
+++ misc/jbehave/jbehave-tests/src/main/java/org/jsystemtest/systemobjects/tests/JBehaveExecutor.java
@@ -0,0 +1,126 @@
+package org.jsystemtest.systemobjects.tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import jsystem.framework.ParameterProperties;
+import jsystem.framework.TestProperties;
+import jsystem.framework.report.Reporter;
+import junit.framework.SystemTestCase4;
+
+import org.jbehave.core.embedder.Embedder;
+import org.jsystemtest.systemobjects.jbehave.JSystemEmbedder;
+import org.jsystemtest.systemobjects.jbehave.JSystemEmbedder.StoryParserType;
+import org.jsystemtest.systemobjects.steps.MySteps;
+import org.jsystemtest.systemobjects.utils.BeanUtils;
+import org.junit.Test;
+
+public class JBehaveExecutor extends SystemTestCase4 {
+
+	private String[] stepsPackages = { "org.jsystemtest.systemobjects.steps" };
+	private String[] storyPaths = { "my.story" };
+	private StoryParserType parserType = StoryParserType.REGEX;
+	private File storyFile;
+
+	/**
+	 * Run all the specified stories using the step classes that are in the
+	 * specified package.
+	 */
+	@Test
+	@TestProperties(name = "Run multiple JBehave stories", paramsInclude = { "stepsPackages", "storyPaths",
+			"parserType" })
+	public void runStories() {
+		// Checking inputs
+		if (null == stepsPackages || stepsPackages.length == 0) {
+			report.report("JBehave steps were not specified - skipping execution", Reporter.WARNING);
+			return;
+		}
+		if (null == storyPaths || storyPaths.length == 0) {
+			report.report("No stories were specified - skipping execution", Reporter.WARNING);
+			return;
+		}
+
+		report.report("About to run stories");
+		Embedder embedder = new JSystemEmbedder(BeanUtils.findClassesInPackages(stepsPackages));
+		embedder.runStoriesAsPaths(Arrays.asList(storyPaths));
+
+	}
+
+	/**
+	 * Run a single JBehave story using the step classes that are in the
+	 * specified package.
+	 */
+	@Test
+	@TestProperties(name = "Run a single JBehave story", paramsInclude = { "stepsPackages", "storyFile", "parserType" })
+	public void runStory() throws IOException {
+		// Checking inputs
+		if (null == stepsPackages || stepsPackages.length == 0) {
+			report.report("JBehave steps were not specified - skipping execution", Reporter.WARNING);
+			return;
+		}
+		if (null == storyFile || !storyFile.exists()) {
+			report.report("Story was not found - skipping execution", Reporter.WARNING);
+			return;
+		}
+
+		report.report("About to run story " + storyFile.getName());
+		Embedder embedder = new JSystemEmbedder(BeanUtils.findClassesInPackages(stepsPackages));
+		List<String> storyList = new ArrayList<String>();
+		storyList.add(storyFile.getCanonicalPath());
+		embedder.runStoriesAsPaths(storyList);
+
+	}
+	
+	
+	@Test
+	public void runJBehaveStory(){
+		List<Class<?>> stepClasses = new ArrayList<Class<?>>();
+		stepClasses.add(MySteps.class);
+		Embedder embedder = new JSystemEmbedder(stepClasses);
+		
+		List<String> stories = new ArrayList<String>();
+		stories.add("my.story");
+		embedder.runStoriesAsPaths(stories);
+		
+	}
+
+	public String[] getStepsPackages() {
+		return stepsPackages;
+	}
+
+	@ParameterProperties(description = "The packages that holds the step classes")
+	public void setStepsPackages(String[] stepsPackages) {
+		this.stepsPackages = stepsPackages;
+	}
+
+	public String[] getStoryPaths() {
+		return storyPaths;
+	}
+
+	@ParameterProperties(description = "The location of the story files")
+	public void setStoryPaths(String[] storyPaths) {
+		this.storyPaths = storyPaths;
+	}
+
+	public File getStoryFile() {
+		return storyFile;
+	}
+
+	@ParameterProperties(description = "JBehave story file")
+	public void setStoryFile(File storyFile) {
+		this.storyFile = storyFile;
+	}
+
+	public StoryParserType getParserType() {
+		return parserType;
+	}
+
+	@ParameterProperties(description = "Which parser type to use")
+	public void setParserType(StoryParserType parserType) {
+		this.parserType = parserType;
+	}
+
+}
